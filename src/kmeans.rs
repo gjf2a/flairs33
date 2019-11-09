a7cbec822ec25bc954a043bcf101a4718ba53cfc
gjf2a <ferrer@hendrix.edu>
2019-11-09 09:18:14 -0600

    Completed implementation of kmeans++. No testing as of yet.

--- src/kmeans.rs
+++ src/kmeans.rs
@@ -2,7 +2,7 @@ use decorum::R64;
 use rand::thread_rng;
 use rand_distr::{Distribution, Uniform};
 use rand::distributions::WeightedIndex;
-use std::ops::Add;
+use std::iter::Sum;
 
 pub struct Kmeans<T, D: Fn(&T,&T) -> R64, M: Fn(&T,usize) -> &T> {
     k: usize,
@@ -11,20 +11,19 @@ pub struct Kmeans<T, D: Fn(&T,&T) -> R64, M: Fn(&T,usize) -> &T> {
     scalar_div: M
 }
 
-impl <T: Clone + Add, D: Fn(&T,&T) -> R64, M: Fn(&T,usize) -> &T> Kmeans<T,D,M> {
+impl <T: Clone + Sum + Eq, D: Fn(&T,&T) -> R64, M: Fn(&T,usize) -> &T> Kmeans<T,D,M> {
     pub fn new(k: usize, data: &Vec<T>, distance: D, scalar_div: M) -> Kmeans<T,D,M> {
         Kmeans {k: k, means: kmeans_iterate(k, data, &distance, &scalar_div), distance: distance, scalar_div: scalar_div}
     }
 
-    fn classify(target: &T, means: &Vec<T>, distance: &D) -> usize {
-        let distances: Vec<(R64,usize)> = (0..means.len())
-            .map(|i| (distance(&target, &means[i]), i))
-            .collect();
-        distances.iter().min().unwrap().1
+    pub fn k(&self) -> usize {self.k}
+
+    pub fn classification(&self, sample: &T) -> usize {
+        classify(sample, &self.means, &self.distance)
     }
 }
 
-fn initial_plus_plus<T: Clone + Add, D: Fn(&T,&T) -> R64>(k: usize, distance: &D, data: &Vec<T>) -> Vec<T> {
+fn initial_plus_plus<T: Clone + Sum + Eq, D: Fn(&T,&T) -> R64>(k: usize, distance: &D, data: &Vec<T>) -> Vec<T> {
     let mut result = Vec::new();
     let mut rng = thread_rng();
     let range = Uniform::new(0, data.len());
@@ -39,8 +38,28 @@ fn initial_plus_plus<T: Clone + Add, D: Fn(&T,&T) -> R64>(k: usize, distance: &D
     result
 }
 
-fn kmeans_iterate<T: Clone + Add, D: Fn(&T,&T) -> R64, M: Fn(&T,usize) -> &T>(k: usize, data: &Vec<T>, distance: &D, scalar_div: &M) -> Vec<T> {
+fn kmeans_iterate<T: Clone + Sum + Eq, D: Fn(&T,&T) -> R64, M: Fn(&T,usize) -> &T>(k: usize, data: &Vec<T>, distance: &D, scalar_div: &M) -> Vec<T> {
     let mut result = initial_plus_plus(k, distance, data);
+    loop {
+        let mut classifications: Vec<Vec<T>> = (0..k).map(|_| Vec::new()).collect();
+        for datum in data {
+            let category = classify(datum, &result, &distance);
+            classifications[category].push(datum.clone());
+        }
+        let prev = result;
 
-    result
+        let sums: Vec<(T,usize)> = classifications.iter().map(|v| (v.iter().map(|t| t.clone()).sum(), v.len())).collect();
+        result = sums.iter().map(|(n,d)| scalar_div(n, *d).clone()).collect();
+
+        if (0..result.len()).all(|i| prev[i] == result[i]) {
+            return result;
+        }
+    }
+}
+
+fn classify<T: Clone + Sum + Eq, D: Fn(&T,&T) -> R64>(target: &T, means: &Vec<T>, distance: &D) -> usize {
+    let distances: Vec<(R64,usize)> = (0..means.len())
+        .map(|i| (distance(&target, &means[i]), i))
+        .collect();
+    distances.iter().min().unwrap().1
 }
