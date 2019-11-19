be82e9ec683076acd0a2faec8ed471e7b2b49acd
gjf2a <ferrer@hendrix.edu>
2019-11-19 08:21:16 -0600

    This version is the state of the code at the moment that I submitted the FLAIRS 33 paper.

--- src/convolutional.rs
+++ src/convolutional.rs
@@ -3,24 +3,6 @@ use crate::kmeans;
 use decorum::R64;
 use crate::euclidean_distance::euclidean_distance;
 
-// Here's the approach:
-// - Use k-means to find k convolution filters. (Imagine using 3x3.)
-//   - Use both images being compared to generate the filters.
-// - For each of the k filters:
-//   - Generate an image based on each pixel's response to the filter.
-//     - Response function:
-//       - Find distance between pixel neighborhood and the filter.
-//       - Somehow scale the distance to a u8, where zero distance yields 255
-//   - Shrink the image by a factor of 2 ("pooling")
-// - Repeat this process once
-//   - Actually, let's try it with both one level and two levels.
-// - Caluclate distance based on the filtered images at the final level.
-//
-//
-// Numbers of images:
-// - Imagine 8 filters
-// - 28x28, 8 x 14x14, 64 x 7x7
-
 const NUM_KERNELS: usize = 8;
 const KERNEL_SIZE: usize = 3;
 const STRIDE: usize = 2;
